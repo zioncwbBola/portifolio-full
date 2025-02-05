7cef0e0cae4fd08263d32ad352056be89af2fed2
ZionGate Cwb <andre.lgnd17165@gmail.com>
2025-02-04 22:40:20 -0300

    new file:   .vscode/settings.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx 	deleted:    src/components/Contact.tsx 	deleted:    src/components/Footer.tsx 	deleted:    src/components/Header.tsx 	deleted:    src/components/Hero.tsx 	deleted:    src/components/MainComponent.tsx 	deleted:    src/components/Services.tsx 	deleted:    src/components/SocialsSidebar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
 import type React from "react" // Added import for React
 
 const inter = Inter({ subsets: ["latin"] })
@@ -17,14 +15,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  
   return (
     <html lang="pt-BR" data-theme="light">
       <body className={inter.className}>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow">{children}</main>
-          <Footer />
-        </div>
+        {children}
       </body>
     </html>
   )
